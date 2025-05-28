0e53d02167e47ac2a84e27f547f8cc28b93444d0
harold <ananbbnn@gmail.com>
2025-05-28 21:40:36 +0800

    不知在做啥

--- todolist/settings.py
+++ todolist/settings.py
@@ -20,12 +20,12 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_KEY')
+SECRET_KEY = 'django-insecure-cb%v(_)x)-k(id9sd7*e2f80ow713@0z$6^o(+v&*pf9%8v+uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 
-load_dotenv(BASE_DIR / ".env")
+
 
 DATABASES = {
     'default': {
